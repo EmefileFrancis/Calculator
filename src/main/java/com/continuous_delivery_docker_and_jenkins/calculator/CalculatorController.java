d2fda96f85d69aebd4c301a436b9051ce731a0b8
Francis <emefilefranklin@gmail.com>
2024-10-05 08:46:35 +0100

    fix: fixes path issue

--- src/main/java/com/continuous_delivery_docker_and_jenkins/calculator/CalculatorController.java
+++ src/main/java/com/continuous_delivery_docker_and_jenkins/calculator/CalculatorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/calculator")
+@RestController
 public class CalculatorController {
     @Autowired
     private CalculatorService calculatorService;
