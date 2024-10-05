40898774b4f767ac0073f143713dfc2e02df85c8
Francis <emefilefranklin@gmail.com>
2024-10-05 11:02:59 +0100

    fix: adds unit test

--- src/test/java/com/continuous_delivery_docker_and_jenkins/calculator/CalculatorTest.java
+++ src/test/java/com/continuous_delivery_docker_and_jenkins/calculator/CalculatorTest.java
@@ -3,7 +3,7 @@ package com.continuous_delivery_docker_and_jenkins.calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
